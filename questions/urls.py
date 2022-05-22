f02676d60ad5dea207ef741b2772e0176e0fb2d7
nawazish <nawazishqadir.odesk@gmail.com>
2022-05-22 23:04:32 +0500

    bootstrap added

--- questions/urls.py
+++ questions/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='main-index'),
+    path('', views.index, name='index'),
     path('generate_paper/', views.generate_paper, name='generate_paper'),
     path('ajax/load-dropdown/', views.load_dropdown_ajax, name='ajax_load_dropdown'),
 ]
